06ccfdcb49ae45d9875c679def6d94c02655bae4
Bek <Bek.Go@outlook.com>
2019-06-23 15:18:44 +0200

    Added Xamarin Forms project

---
+++ SignalRXamarin/SignalRXamarin/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalRXamarin.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ SignalRXamarin/SignalRXamarin/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalRXamarin.Models;
+using SignalRXamarin.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SignalRXamarin.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private readonly HubConnection _hubConnection;
+        public MainPageViewModel()
+        {
+            _hubConnection = new HubConnectionBuilder().WithUrl("http://localhost:49970/ProductsHub").Build();
+            ConnectCommand = new RelayCommand(Connect);
+
+        }
+        private async void Connect()
+        {
+            _hubConnection.On<Product>("Add", p =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Products.Add(p);
+                });
+            }
+            );
+            _hubConnection.On<int>("Delete", id => Device.BeginInvokeOnMainThread(() => Products.Remove(_products.FirstOrDefault(p => p.Id == id))));
+            await _hubConnection.StartAsync();
+        }
+        public RelayCommand ConnectCommand { get; set; }
+        private ObservableCollection<Product> _products = new ObservableCollection<Product>();
+        public ObservableCollection<Product> Products
+        {
+            get => _products;
+            set => this.SetField(ref _products, value);
+        }
+    }
+}
